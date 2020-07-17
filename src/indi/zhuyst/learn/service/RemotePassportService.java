717e26ceb0a8d374d9c9f5d5aaec2d466e551a28
lilun01 <1547709975@qq.com>
2020-07-17 15:39:25 +0800

    Update RemotePassportService.java

--- src/indi/zhuyst/learn/service/RemotePassportService.java
+++ src/indi/zhuyst/learn/service/RemotePassportService.java
@@ -4,25 +4,27 @@ import java.util.Random;
 
 public class RemotePassportService {
 
-    public boolean checkAuth(int uid){
+    public String checkAuth(int uid){
         boolean flag;
 
         System.out.println("黑名单 - 验证开始");
         try {
             Thread.sleep(3000);
-            flag = new Random().nextBoolean();
+            Thread.currentThread().setName("验证黑名单的线程");
+            //flag = new Random().nextBoolean();
+            flag = true;
         } catch (InterruptedException e) {
             System.out.println("黑名单 - 验证终止");
-            return false;
+            return Thread.currentThread().getName() + false;
         }
 
         if(flag){
             System.out.println("黑名单 - 验证成功");
-            return true;
+            return  Thread.currentThread().getName() + "true";
         }
         else {
             System.out.println("黑名单 - 验证失败");
-            return false;
+            return Thread.currentThread().getName() + false;
         }
     }
 }
