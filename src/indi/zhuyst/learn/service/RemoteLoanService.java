2b61693b98f63f4b94a2b67a40536dd3b0ff79a1
lilun01 <1547709975@qq.com>
2020-07-17 15:39:02 +0800

    Update RemoteLoanService.java

--- src/indi/zhuyst/learn/service/RemoteLoanService.java
+++ src/indi/zhuyst/learn/service/RemoteLoanService.java
@@ -2,27 +2,28 @@ package indi.zhuyst.learn.service;
 
 public class RemoteLoanService {
 
-    public boolean checkAuth(int uid){
+    public String checkAuth(int uid){
         boolean flag;
 
         System.out.println("不良贷款 - 验证开始");
         try {
             Thread.sleep(1000);
+            Thread.currentThread().setName("不良贷款的线程");
             // 这里让时间最短的直接失败，方便查看测试结果
             // flag = new Random().nextBoolean();
-            flag = false;
+            flag = true;
         } catch (InterruptedException e) {
             System.out.println("不良贷款 - 验证终止");
-            return false;
+            return Thread.currentThread().getName() + false;
         }
 
         if(flag){
             System.out.println("不良贷款 - 验证成功");
-            return true;
+            return  Thread.currentThread().getName() +  "true";
         }
         else {
             System.out.println("不良贷款 - 验证失败");
-            return false;
+            return Thread.currentThread().getName() +  false;
         }
     }
 }
